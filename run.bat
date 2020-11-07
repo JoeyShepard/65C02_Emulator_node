cb48d298eb3d69a64fd109a213091981b808c884
Joey Shepard <joey.m.shepard@gmail.com>
2020-11-07 10:30:53 -0500

    initial commit

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+REM https://futurestud.io/tutorials/node-js-increase-the-memory-limit-for-your-process
+REM node --max-old-space-size=2048 .
+
+REM always run from current directory!
+node --max-old-space-size=2048 "%~dp0\"
